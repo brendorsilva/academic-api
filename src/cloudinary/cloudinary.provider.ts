0df8814d29c9d46baebf2e5a34b97da01473a132
brendorsilva <brendorocha@outlook.com>
2026-04-07 21:52:45 -0300

    fix: corrige a chave da API do Cloudinary para usar a variável de ambiente correta

--- src/cloudinary/cloudinary.provider.ts
+++ src/cloudinary/cloudinary.provider.ts
@@ -5,7 +5,7 @@ export const CloudinaryProvider = {
   useFactory: () => {
     return cloudinary.config({
       cloud_name: process.env.CLOUDINARY_NAME,
-      api_key: process.env.CLOUDINARY_API_KEY,
+      api_key: process.env.CLAUDINARY_KEY,
       api_secret: process.env.CLOUDINARY_API_SECRET,
     });
   },
